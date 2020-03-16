fecfab48af1a07e522a1a1f438fac375ad567d6a
s-davies <swdavies.contact@gmail.com>
2020-03-15 17:26:17 -0700

    add scripts

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World"));
+app.get("/", (req, res) => res.send("Server has updated"));
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
