b4bbd2bf2e8d7ab588ef523792952348ef6d5c33
s-davies <swdavies.contact@gmail.com>
2020-03-16 01:36:50 -0700

    setup user auth

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
+const mongoose = require('mongoose');
 const express = require("express");
 const app = express();
+const db = require('./config/keys').mongoURI;
+const passport = require('passport');
+const users = require("./routes/api/users");
+const bodyParser = require('body-parser')
 
-app.get("/", (req, res) => res.send("Server has updated"));
+mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then(() => console.log("Connected to MongoDB successfully"))
+    .catch(err => console.log(err));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
+app.use("/api/users", users);
---
+++ frontend/src/components/main/main_page.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class MainPage extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Chores App</h1>
+            </div>
+        );
+    }
+}
+
+export default MainPage;
