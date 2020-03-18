fbd07835e874058887fba131c9208f923a613c09
Sara Sampson <sarasampson.sls@gmail.com>
2020-03-18 09:53:02 -0700

    Lengthen user session

--- routes/api/users.js
+++ routes/api/users.js
@@ -82,7 +82,7 @@ router.post('/login', (req, res) => {
                             payload,
                             keys.secretOrKey,
                             // Tell the key to expire in one hour
-                            { expiresIn: 3600 },
+                            { expiresIn: 86400 },
                             (err, token) => {
                                 res.json({
                                     success: true,
