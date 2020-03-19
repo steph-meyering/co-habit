2dabf61443138d3fcf1400e000896db6d9e273ad
Sara Sampson <sarasampson.sls@gmail.com>
2020-03-18 19:39:05 -0700

    Add auto redirect given bad path

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,21 +1,25 @@
-import React from 'react';
-import { AuthRoute, ProtectedRoute } from '../util/route_util';
-import { Route, Switch } from 'react-router-dom';
-import SplashPage from './splash/splash_page';
-import DashboardContainer from './dashboard/dashboard_container';
+import React from "react";
+import { AuthRoute, ProtectedRoute } from "../util/route_util";
+import { Route, Switch, Redirect } from "react-router-dom";
+import SplashPage from "./splash/splash_page";
+import DashboardContainer from "./dashboard/dashboard_container";
 import "../styles/application.scss";
 import BillsIndexContainer from "./bills/bills_index_container";
-import ChoresContainer from "./chores/chores_container"
-import NavBar from "./nav/nav"
+import ChoresContainer from "./chores/chores_container";
+import NavBar from "./nav/nav";
 
 const App = () => (
-    <div className="app">
-        <Route path="/" component={NavBar} />
-        <AuthRoute exact path="/" component={SplashPage} />
-        <ProtectedRoute exact path="/dashboard" component={DashboardContainer} />
-        <ProtectedRoute exact path="/chores" component={ChoresContainer} />
-        <ProtectedRoute exact path="/bills" component={BillsIndexContainer} />
-    </div>
+  <div className="app">
+    <Route path="/" component={NavBar} />
+    <Switch>
+      <AuthRoute exact path="/" component={SplashPage} />
+      <ProtectedRoute exact path="/dashboard" component={DashboardContainer} />
+      <ProtectedRoute exact path="/chores" component={ChoresContainer} />
+      <ProtectedRoute exact path="/bills" component={BillsIndexContainer} />
+      {/* Default redirect given bad path */}
+      <Redirect path="*" to="/dashboard" />
+    </Switch>
+  </div>
 );
 
 export default App;
