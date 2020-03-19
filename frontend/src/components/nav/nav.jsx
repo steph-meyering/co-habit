ca1a0080ddb60223ada3ea8c1a5d08936ae92d76
Sara Sampson <sarasampson.sls@gmail.com>
2020-03-18 19:34:53 -0700

    Add navbar links

--- frontend/src/components/nav/nav.jsx
+++ frontend/src/components/nav/nav.jsx
@@ -1,17 +1,57 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
 import { connect } from "react-redux";
+import { logout } from "../../actions/session_actions";
 
 class NavBar extends React.Component {
   render() {
     return (
       <div>
-        <Link to="/dashboard">
-          <img src="/logo.png" height="50px" />
-        </Link>
+        <div className="logo-container">
+          <Link to="/dashboard">
+            <img src="/logo.png" height="50px" />
+          </Link>
+        </div>
+        <div className="nav-item">
+          <NavLink to="/" activeClassName="active-navlink" className="navlink">
+            Home
+          </NavLink>
+        </div>
+        <div
+          className="nav-item"
+          activeClassName="active-navlink"
+          className="navlink"
+        >
+          <NavLink to="/chores">Chores</NavLink>
+        </div>
+        <div
+          className="nav-item"
+          activeClassName="active-navlink"
+          className="navlink"
+        >
+          <NavLink to="/bills">Bills</NavLink>
+        </div>
+        <div
+          className="nav-item"
+          activeClassName="active-navlink"
+          className="navlink"
+        >
+          <NavLink to="/settings">Settings</NavLink>
+        </div>
+        <div>
+          <button onClick={this.props.logout}>Log Out</button>
+        </div>
       </div>
     );
   }
 }
 
-export default NavBar;
+const mapStateToProps = ({ session }) => ({
+  currentUser: session.user
+});
+
+const mapDispatchToProps = (dispatch, ownProps) => ({
+  logout: () => dispatch(logout())
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(NavBar);
