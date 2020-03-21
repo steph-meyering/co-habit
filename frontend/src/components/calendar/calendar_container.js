9a329e36d033135458025de7203bb9e369b7ad50
App Academy Student <student@appacademy.io>
2020-03-20 22:55:29 -0700

    colors working on calendar

--- frontend/src/components/calendar/calendar_container.js
+++ frontend/src/components/calendar/calendar_container.js
@@ -3,6 +3,7 @@ import HouseholdCalendar from './calendar';
 import { getEvents, createEvent, updateEvent, deleteEvent } from '../../actions/event_actions';
 import { fetchChores } from "../../actions/chore_actions";
 import moment from "moment";
+import { getUsers } from '../../actions/user_actions';
 
 const mapStateToProps = (state) => {
   let chores = Object.values(state.entities.chores);
@@ -30,9 +31,19 @@ const mapStateToProps = (state) => {
     }
   }
 
+  let allEvents = Object.values(state.entities.events).concat(dueDateEvents);
+  const colors = ["#F4976C", "#FBE8A6", "#303C6C", "B4DFE5", "D2FDFF"];
+  const users = Object.keys(state.entities.users);
+  if (users.length > 0) {
+    for (let i = 0; i < allEvents.length; i++) {
+      const event = allEvents[i];
+      event.color = colors[users.indexOf(event.author)]
+    }
+  }
+
   return {
     currentUser: state.session.user,
-    events: Object.values(state.entities.events).concat(dueDateEvents),
+    events: allEvents,
     errors: state.errors.session
   };
 };
@@ -43,7 +54,8 @@ const mapDispatchToProps = (dispatch) => {
     createEvent: (event) => dispatch(createEvent(event)),
     updateEvent: (event) => dispatch(updateEvent(event)),
     deleteEvent: (eventId) => dispatch(deleteEvent(eventId)),
-    fetchChores: () => dispatch(fetchChores())
+    fetchChores: () => dispatch(fetchChores()),
+    getUsers: (householdId) => dispatch(getUsers(householdId)),
   }
 }
 
