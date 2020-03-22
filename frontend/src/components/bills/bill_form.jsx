c80771349ba862a1acb0d931717dbb3f14671cec
Steph Meyering <stephane.meyering@gmail.com>
2020-03-22 15:00:18 -0700

    add error handling for bills

--- frontend/src/components/bills/bill_form.jsx
+++ frontend/src/components/bills/bill_form.jsx
@@ -36,25 +36,35 @@ class BillForm extends React.Component {
         [field]: e.currentTarget.value
       });
   }
-
+  
   render() {
+    // let billErrors = null
+    // debugger
+    // if (!this.props.errors === "undefined"){
+    // debugger
+    //   billErrors = Object.values(this.props.errors.bill)
+    // }
     return (
-      <div className='bill-form'>Add a new bill
-        <form onSubmit={this.handleSubmit}>
-          <h2>What is this bill for?</h2>
-          <input 
-            type="text" 
-            value={this.state.title} 
-            onChange={this.update('title')}/>
-          <br />
-          <h2>Bill total:</h2>
-          <input 
-            type="text" 
-            value={this.state.amount} 
-            onChange={this.update('amount')}/>
-            <br/>
-          <button type="submit">Save this bill</button>
-        </form>
+      <div>
+        <div className='bill-form'>Add a new bill
+          <form onSubmit={this.handleSubmit}>
+            <h2>What is this bill for?</h2>
+            <input 
+              type="text" 
+              value={this.state.title} 
+              onChange={this.update('title')}/>
+            <br />
+            <h2>Bill total:</h2>
+            <input 
+              type="text" 
+              value={this.state.amount} 
+              onChange={this.update('amount')}/>
+            <button type="submit">Save this bill</button>
+          </form>
+        </div>
+        <ol className='bill-errors'>
+          {/* {billErrors} */}
+        </ol>
       </div>
     );
   }
