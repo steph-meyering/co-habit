92423f3263008b50dd24e6595c393e08be68ee2b
Steph Meyering <stephane.meyering@gmail.com>
2020-03-21 19:42:29 -0700

    update reducer case statement to receive REMOVE_BILL

--- frontend/src/reducers/pie_chart_reducer.js
+++ frontend/src/reducers/pie_chart_reducer.js
@@ -1,22 +1,29 @@
-import { RECEIVE_BILLS, RECEIVE_BILL } from "../actions/bill_actions";
-import { RECEIVE_USERS } from "../actions/user_actions";
+import { REMOVE_BILL, RECEIVE_BILL } from "../actions/bill_actions";
 import { UPDATE_PIE_CHART } from "../actions/pie_chart_actions";
 
 const PieChartReducer = (state = [], action) => {
     Object.freeze(state);
     let nextState = Object.assign([], state);
     switch (action.type) {
-      case UPDATE_PIE_CHART:
-        return action.data;
-      case RECEIVE_BILL:
-        nextState.forEach(obj => {
-          if (obj.userId === action.bill.data.user) {
-            obj.value += action.bill.data.amount;
-          }
-        });
-        return nextState;
-      default:
-        return state;
+        case UPDATE_PIE_CHART:
+            return action.data;
+        case RECEIVE_BILL:
+            nextState.forEach(obj => {
+            if (obj.userId === action.bill.data.user) {
+                obj.value += action.bill.data.amount;
+            }
+            });
+            return nextState;
+        case REMOVE_BILL:
+            nextState.forEach(obj => {
+                debugger
+            if (obj.userId === action.bill.user) {
+                obj.value -= action.bill.amount;
+            }
+            });
+            return nextState;
+        default:
+            return state;
     }
 };
 
